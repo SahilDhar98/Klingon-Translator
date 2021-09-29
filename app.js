3a7ac7bc07ff642728c879aef71bb8b8fb019c89
Sahil Dhar <sahildhar98@gmail.com>
2021-09-30 00:20:49 +0530

    js done

--- app.js
+++ app.js
@@ -5,4 +5,30 @@ var outputdiv = document.querySelector("#txt-output");
 
 
 
+btntranslate.addEventListener("click" , clickeventhandler);
+
+
+var serverURL = "https://api.funtranslations.com/translate/klingon.json?text=";
+// Url creator
+function urlcreator(input){
+    return serverURL + input ;
 }
+
+function errorHandler(error){
+    alert("error occured", error);
+}
+// Event handler 
+function clickeventhandler(){
+    console.log("Button clicked");
+    var txt = txtinput.value;
+    console.log(txt);
+    console.log(urlcreator(txt));
+    var finalURL = urlcreator(txt);
+    fetch(finalURL)
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputdiv.innerText = translatedText; 
+            
+    })
+        .catch(errorHandler)}
