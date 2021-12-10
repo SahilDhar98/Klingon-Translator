f422da9774b149c0815e64aeb18690bd12c9fb63
Sahil Dhar <80975472+SahilDhar98@users.noreply.github.com>
2021-12-10 14:21:39 +0530

    Updated link

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var outputdiv = document.querySelector("#txt-output");
 btntranslate.addEventListener("click" , clickeventhandler);
 
 
-var serverURL = "https://api.funtranslations.com/translate/morse.json?text=";
+var serverURL = "https://api.funtranslations.com/translate/klingon.json?text=";
 // Url creator
 function urlcreator(input){
     return serverURL + input ;
